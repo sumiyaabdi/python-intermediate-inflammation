8ee6a3079ebcead6873b1037c08f18aed7b5877e
sumiyaabdi <20847637+sumiyaabdi@users.noreply.github.com>
2023-03-21 15:23:38 +0100

    add tests for daily_min() and aily_max()

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 
 def test_daily_mean_zeros():
@@ -29,3 +30,45 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_daily_mean_shape():
+    """Test that mean function works for an array of positive integers."""
+    from inflammation.models import daily_mean
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([3, 4])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_mean(test_input).shape[0], test_input.shape[1])
+
+def test_daily_max_ints():
+    """Test that mean function works for an array of positive integers."""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[10, 2, 3, 4],
+                           [5, 65, 7, 8],
+                           [9, 10, 11, 1]])
+    test_result = np.array([10, 65, 11, 8])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_min_ints():
+    """Test that mean function works for an array of positive integers."""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[10, 2, 3, 4],
+                           [5, 65, 7, 8],
+                           [9, 10, 11, 1]])
+    test_result = np.array([5, 2, 3, 1])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
+    
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
